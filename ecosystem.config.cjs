113022620e47010559f1b3b8281a75ae5643fecd
wflixu <wflixu@88.com>
2024-08-28 21:31:48 +0800

    chore:update pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,6 +5,10 @@ module.exports = {
         port: '3456',
         exec_mode: 'cluster',
         instances: '1',
+        env_production: {
+          NODE_ENV: 'production',
+          PORT: 3456,
+        },
         script: './.output/server/index.mjs'
       }
     ]
