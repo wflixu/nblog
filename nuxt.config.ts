348e37e41981a3854b2023ff4d22fddd7ca7caa9
wflixu <wflixu@88.com>
2024-08-28 18:06:59 +0800

    chore: add logo image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   routeRules: {
-    '/': { prerender: true },
     '/api/**': { cors: true },
   },
   runtimeConfig: {
