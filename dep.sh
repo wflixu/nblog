c8c11d0ead1b97f87fd084d5d9f540b84d84534d
wflixu <wflixu@88.com>
2024-08-30 12:09:58 +0800

    chore: deploy shell

---
+++ dep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export NOTION_TOKEN=""
+export DATABASE_ID="12476b6af58e41738258b896720a8f89"
+export API_HOST="https://api.notion.com/v1"
+
+
+echo "start pulling code" 
+git  pull 
+
+echo "start installing dependencies" 
+pnpm install 
+
+echo $NOTION_TOKEN 
+
+echo "start building" 
+pnpm run build 
+
+
